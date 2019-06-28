64f925620a2580a7840a8ebf372a81278537f523
masterg24 <52319050+masterg24@users.noreply.github.com>
2019-06-28 15:18:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
-
-
 client.on("ready", () => {
 let channel =     client.channels.get("594089901054623754")
 setInterval(function() {
