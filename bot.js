86665a2296cc8395db91888a87f4a0087ae42019
masterg24 <52319050+masterg24@users.noreply.github.com>
2019-06-28 15:30:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', message => {
-    if(message.content.startsWith('-'+'say'))
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do this.");
-  let botmsg = args.join(" ");
-  message.channel.send(botmsg);
-  message.delete(1);
-})
 console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("594089901054623754")
@@ -16,11 +9,4 @@ channel.send(`**Shanks Shanks Shanks ShanksShanks Shanks Shanks Shanks Shanks Sh
 }, 40)
 })
 
-client.on('message', message => {
-    if(message.content.startsWith('-'+'say'))
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do this.");
-  let botmsg = args.join(" ");
-  message.channel.send(botmsg);
-  message.delete(1);
-})
 client.login(process.env.BOT_TOKEN);
