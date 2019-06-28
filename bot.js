212cfdad7ea05217589679c98bf3e0823def4528
masterg24 <52319050+masterg24@users.noreply.github.com>
2019-06-28 15:14:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,5 +9,11 @@ setInterval(function() {
 channel.send(`**Shanks Shanks Shanks ShanksShanks Shanks Shanks Shanks Shanks Shanks Shanks Shanks**`);
 }, 30)
 })
-
+client.on('message', message => {
+    if(message.content.startsWith(';'+'say'))
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do this.");
+  let botmsg = args.join(" ");
+  message.channel.send(botmsg);
+  message.delete(1);
+})
 client.login(process.env.BOT_TOKEN);
